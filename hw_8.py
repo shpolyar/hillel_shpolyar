cef0724880e3176f16e91706dd220b3e2a74963d
shpolyar <dasha.shpolyar@gmail.com>
2022-05-31 22:15:01 +0300

    Change hw8 and hw9

--- hw_8.py
+++ hw_8.py
@@ -4,7 +4,5 @@ petia = int(input('Рост Пети: '))
 height = [random.randint(150, 200) for _ in range(15)] + [petia]
 height.sort(reverse=True)
 index = height.index(petia)
-height.sort()
-print(height)
-print(f'Петя должен встать {15 - index + 1} в строй')
+print(f'Петя должен встать {len(height) - index} в строй')
 
--- hw_9.py
+++ hw_9.py
@@ -2,7 +2,7 @@ import random
 my_list = [random.randint(0, 100) for _ in range(20)]
 print(my_list)
 count = 0
-for i in my_list[1:19]:
+for i in my_list[1:len(my_list) - 1]:
     index = my_list.index(i)
     if i > my_list[index - 1] and i > my_list[index + 1]:
         count += 1
