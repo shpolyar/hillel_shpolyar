4e095ec574b7f733050ac0b19424d3d0c09e3cb6
shpolyar <dasha.shpolyar@gmail.com>
2022-05-31 13:27:29 +0300

    edit hw_6

--- hw_6.py
+++ hw_6.py
@@ -1,33 +1,31 @@
 n = int(input('Введите число: '))
 if n != 0:
-    items = []
-    while n != 0:
-        items.append(n)
-        n = int(input('Введите число: '))
     total = 0
     quantity = 0
-    max_num = 0
-    min_num = 0
     number_of_even = 0
     number_of_odd = 0
-    for i in items:
-        total += i
+    max_num = 0
+    min_num = n
+    while n != 0:
+        total += n
         quantity += 1
-        if i % 2:
+        if n % 2:
             number_of_odd += 1
         else:
             number_of_even += 1
-    items.sort()
-    min_num = items[0]
-    max_num = items[quantity - 1]
+        if n > max_num:
+            max_num = n
+        elif n < min_num:
+            min_num = n
+        n = int(input('Введите число: '))
     average = total / quantity
-    print(f"Количество введеных чисел: {quantity}\n"
-          f"Сума введеных чисел: {total}\n"
-          f"Среднее арефметическое введеных чисел: {average}\n"
-          f"Максимальное значение: {max_num}\n"
-          f"Минимальное значение: {min_num}\n"
-          f"Количество парных чисел: {number_of_even}\n"
-          f"Количесво непарных чисел: {number_of_odd}")
+    print("Количество введеных чисел: ", quantity)
+    print("Сума введеных чисел: ", total)
+    print("Среднее арефметическое введеных чисел: ", average)
+    print("Максимальное значение: ", max_num)
+    print("Минимальное значение: ", min_num)
+    print("Количество парных чисел: ", number_of_even)
+    print("Количесво непарных чисел: ", number_of_odd)
 else:
     print('0 был введен сразу\nДля вычислений нужны натуральные числа')
 
