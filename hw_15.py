458efb4344f5c1ecfbc3247bbdc0809d3fe12270
shpolyar <dasha.shpolyar@gmail.com>
2022-06-10 17:46:16 +0300

    add hw15

--- hw_15.py
+++ hw_15.py
@@ -1,20 +1,17 @@
-def arithmetic(num_1, num_2, operator):
-    res = 0
-    if operator == '+':
-        res = num_1 + num_2
-    elif operator == '-':
-        res = num_1 - num_2
-    elif operator == '*':
-        res = num_1 * num_2
-    elif operator == '/':
-        res = num_1 / num_2
-    else:
-        print("Неизвестная операция")
-    return res
+import operator
+
+my_dict = {'+': operator.add,
+           '-': operator.sub,
+           '*': operator.mul,
+           '/': operator.truediv
+           }
+
+
+def arithmetic(num_1, num_2, operation):
+    return my_dict.get(operation, "Неизвестная операция")(num_1, num_2)
 
 
 num_1 = int(input('Первое число: '))
 num_2 = int(input('Второе число: '))
-operator = input('Операция: ')
-print(arithmetic(num_1, num_2, operator))
-
+operation = input('Операция: ')
+print(arithmetic(num_1, num_2, operation))
