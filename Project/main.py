688993159dc97189e11ced2732ade1f598fe2e4a
shpolyar <dasha.shpolyar@gmail.com>
2022-06-15 19:20:43 +0300

    Edit main.py

--- Project/main.py
+++ Project/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 users = {'Charlie': '12345',
          'Steve': 'apple',
          'Kate': '0000',
@@ -5,13 +7,19 @@ users = {'Charlie': '12345',
          }
 
 
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', help=' Your username')
+    parser.add_argument('--password', help='Your password')
+    return parser.parse_args()
+
+
 def decorator(func):
-    def wrapper(name, password):
+    def wrapper(*args, **kwargs):
         if not check_password(name, password):
             print("Не правильное Имя или Пароль")
             return False
-        return func
-
+        return func(*args, **kwargs)
     return wrapper
 
 
@@ -26,9 +34,21 @@ def check_password(name, password):
 
 if __name__ == "__main__":
     n = 3
-    while n > 0:
+    if parse().name is None:
         name = input("Введите Имя: ")
+    else:
+        name = parse().name
+
+    if parse().password is None:
         password = input("Введите Пароль: ")
+    else:
+        password = parse().password
+    # чувствую, что предыдущих два if надо бы заменить функцией, но не придумала как ее написать
+
+    while n > 0:
+        if n < 3:
+            name = input("Введите Имя: ")
+            password = input("Введите Пароль: ")
         if login(name, password):
             print("Вы в системе!")
             break
