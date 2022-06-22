f77d5da9dd27e84d2e3581d8f42e1e382f987a89
shpolyar <dasha.shpolyar@gmail.com>
2022-06-22 16:50:17 +0300

    Edit main.py

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,8 @@
 import argparse
+from datetime import timedelta, datetime
+
+wrong_try = datetime(2022, 6, 22, 16, 18)
+now = datetime(2022, 6, 22, 16, 20)
 
 users = {'Charlie': '12345',
          'Steve': 'apple',
@@ -20,6 +24,7 @@ def decorator(func):
             print("Не правильное Имя или Пароль")
             return False
         return func(*args, **kwargs)
+
     return wrapper
 
 
@@ -30,6 +35,12 @@ def login(name, password):
 
 def check_password(name, password):
     return users.get(name) == password
+
+
+def blocking(t1, t2):
+    r = t2 - t1
+    if r < timedelta(minutes=5):
+        print(f"Вы заблокированы! Следующая попытка через {timedelta(minutes=5) - r} мин")
 
 
 if __name__ == "__main__":
@@ -43,7 +54,6 @@ if __name__ == "__main__":
         password = input("Введите Пароль: ")
     else:
         password = parse().password
-    # чувствую, что предыдущих два if надо бы заменить функцией, но не придумала как ее написать
 
     while n > 0:
         if n < 3:
@@ -57,3 +67,4 @@ if __name__ == "__main__":
             print(f"У вас осталось попыток: {n}")
         else:
             print("Попытки истекли!")
+            blocking(wrong_try, now)
