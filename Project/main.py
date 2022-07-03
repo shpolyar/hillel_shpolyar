401aca1e412d397a280ab429c233be601a0db05f
shpolyar <dasha.shpolyar@gmail.com>
2022-07-03 20:34:37 +0300

    Edit main.py

--- Project/main.py
+++ Project/main.py
@@ -8,6 +8,10 @@ class UserDoesNotExist(Exception):
     pass
 
 
+class WrongPassword(Exception):
+    pass
+
+
 def read_file(file_name):
     try:
         with open(file_name) as f:
@@ -24,15 +28,12 @@ def write_file(file_name, data):
         print(e)
 
 
-def check_password(name, password):
-    users = read_file('data.json')
-    if users.get(name) is not None:
-        if not users.get(name).get('password') == password:
-            raise UserDoesNotExist("Неверный Пароль")
-        else:
-            return True
-    else:
-        raise UserDoesNotExist("Данного имени не существует")
+def registration():
+    d = read_file('data.json')
+    n = input('Введите Имя: ')
+    p = input('Введите Пароль: ')
+    d[n] = {'password': str(p), 'last_try': "0000-00-00 00:00"}
+    write_file('data.json', d)
 
 
 def parse():
@@ -40,15 +41,6 @@ def parse():
     parser.add_argument('--name', help=' Your username')
     parser.add_argument('--password', help='Your password')
     return parser.parse_args()
-
-
-def is_block(name):
-    users = read_file('data.json')
-    r = datetime.now() - datetime.strptime(users[name]['last_try'], "%Y-%m-%d %H:%M")
-    if r < timedelta(minutes=5):
-        print(f"Вы заблокированы! Следующая попытка через {timedelta(minutes=5) - r} мин")
-        exit()
-    return True
 
 
 def last_try(name):
@@ -60,13 +52,42 @@ def last_try(name):
         pass
 
 
+def check_password(name, password):
+    users = read_file('data.json')
+    if users.get(name) is not None:
+        if not users.get(name).get('password') == password:
+            raise WrongPassword("Неверный Пароль")
+        else:
+            return True
+    else:
+        raise UserDoesNotExist("Данного имени не существует")
+
+
+def is_block(name):
+    users = read_file('data.json')
+    try:
+        r = datetime.now() - datetime.strptime(users[name]['last_try'], "%Y-%m-%d %H:%M")
+        if r < timedelta(minutes=5):
+            print(f"Вы заблокированы! Следующая попытка через {timedelta(minutes=5) - r} мин")
+            exit()
+    except KeyError:
+        pass
+        return False
+    return True
+
+
 def decorator(func):
     def wrapper(*args, **kwargs):
         try:
             check_password(name, password)
-        except UserDoesNotExist as e:
+        except WrongPassword as e:
             print(e)
             return False
+        except UserDoesNotExist as e:
+            print(e)
+            answer = input('Желаете зарегистрироваться? (yes/no):\n')
+            if answer == 'yes':
+                registration()
         if not is_block(name):
             return False
         return func(*args, **kwargs)
